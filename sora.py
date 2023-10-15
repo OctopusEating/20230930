e77fec548f96065387e259decdac773941df4f09
“OctopusEating” <s.sora.0106@gmail.com>
2023-10-15 18:10:01 +0900

    first commit

---
+++ sora.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello everyone, my name is ”super man”"
+
+@app.route("/sora")
+def indox():
+    return "What do you like subject?"
+
+@app.route("/banana_cat")
+
+def indx():
+    return "It's good banana"
+
+@app.route("/apple_dog")
+def html():
+    return render_template("banana.html")
+
+app.run()
